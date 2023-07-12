7d9a26d62a976beb5e3fd41d515f16f7f3aa5233
vtb4055920 <vtb4055920@region.vtb.ru>
2023-07-12 12:08:48 +0300

    add Deque

---
+++ src/main/java/lessons/OrderedList/OrderedList.java
@@ -0,0 +1,158 @@
+package lessons.OrderedList;
+
+import java.util.*;
+
+class Node<T> {
+    public T value;
+    public Node<T> next, prev;
+
+    public Node(T _value) {
+        value = _value;
+        next = null;
+        prev = null;
+    }
+}
+
+public class OrderedList<T> {
+    public Node<T> head, tail;
+    private boolean _ascending;
+    private int size;
+
+    public OrderedList(boolean asc) {
+        head = null;
+        tail = null;
+        _ascending = asc;
+    }
+
+    public int compare(T v1, T v2) {
+        int c = 0;
+
+        if (v1 instanceof String) {
+            c = (((String) v1).trim()).compareTo(((String) v2).trim());
+            c = Integer.compare(c, 0);
+        } else {
+            c = Integer.compare((Integer) v1, (Integer) v2);
+        }
+
+        if (!_ascending) {
+            return -1 * c;
+        }
+
+        return c;
+    }
+
+    public void add(T value) {
+        Node<T> newNode = new Node<>(value);
+        Node<T> current = this.head;
+        Node<T> previous = null;
+
+        if (this.head == null) {
+            this.head = newNode;
+            this.tail = newNode;
+            size++;
+            return;
+        }
+
+        while (current != null && compare(value, current.value) > 0) {
+            previous = current;
+            current = current.next;
+        }
+
+        if (current == null) {
+            newNode.prev = previous;
+            newNode.next = null;
+            previous.next = newNode;
+            this.tail = newNode;
+            size++;
+            return;
+        }
+
+        linkBefore(value, current);
+
+    }
+
+    public Node<T> find(T val) {
+        Node<T> node = this.head;
+        while (node != null && compare(val, node.value) > 0) {
+            node = node.next;
+        }
+
+        if (compare(val, node.value) == 0) {
+            return node;
+        }
+
+        return null;
+    }
+
+    public void delete(T val) {
+        Node<T> toDelete = find(val);
+        if (toDelete != null) {
+            unlink(toDelete);
+        }
+    }
+
+    public void clear(boolean asc) {
+        _ascending = asc;
+        Node<T> node = this.head;
+        while (node != null) {
+            Node<T> next = node.next;
+            node.next = null;
+            node.prev = null;
+            node.value = null;
+            node = next;
+        }
+        this.head = this.tail = null;
+        size = 0;
+    }
+
+    public int count() {
+        return size;
+    }
+
+    ArrayList<Node<T>> getAll() {
+        ArrayList<Node<T>> r = new ArrayList<Node<T>>();
+        Node<T> node = head;
+        while (node != null) {
+            r.add(node);
+            node = node.next;
+        }
+        return r;
+    }
+
+    private void linkBefore(T val, Node<T> n) {
+        Node<T> prev = n.prev;
+        Node<T> newNode = new Node<>(val);
+        newNode.next = n;
+        n.prev = newNode;
+
+        if (prev == null) {
+            this.head = newNode;
+        } else {
+            newNode.prev = prev;
+            newNode.next = prev.next;
+            prev.next = newNode;
+            newNode.next.prev = newNode;
+        }
+        this.size++;
+    }
+
+    private void unlink(Node<T> node) {
+        Node<T> prev = node.prev;
+        Node<T> next = node.next;
+
+        if (prev == null) {
+            this.head = next;
+        } else {
+            prev.next = next;
+            node.prev = null;
+        }
+
+        if (next == null) {
+            this.tail = prev;
+        } else {
+            next.prev = prev;
+            node.next = null;
+        }
+        size--;
+    }
+}
---
+++ src/test/java/lessons/OrderedList/OrderedListTest.java
@@ -0,0 +1,122 @@
+package lessons.OrderedList;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class OrderedListTest {
+
+    @Test
+    void addAsc() {
+        OrderedList<Integer> list = new OrderedList<>(true);
+        list.add(1);
+        list.add(10);
+        list.add(5);
+        list.add(2);
+        list.add(7);
+
+        assertEquals(5, list.count());
+    }
+
+    @Test
+    void addDesc() {
+        OrderedList<Integer> list = new OrderedList<>(false);
+        list.add(1);
+        list.add(10);
+        list.add(5);
+        list.add(2);
+        list.add(7);
+
+        assertEquals(5, list.count());
+    }
+
+    @Test
+    void findInDesc() {
+        OrderedList<Integer> list = new OrderedList<>(false);
+        list.add(1);
+        list.add(10);
+        list.add(5);
+        list.add(2);
+        list.add(7);
+
+        Node<Integer> integerNode = list.find(5);
+
+        assertEquals(5, integerNode.value);
+    }
+
+    @Test
+    void findInDescWhenNotExists() {
+        OrderedList<Integer> list = new OrderedList<>(false);
+        list.add(1);
+        list.add(10);
+        list.add(5);
+        list.add(2);
+        list.add(7);
+
+        Node<Integer> integerNode = list.find(500);
+
+        assertNull(integerNode);
+    }
+
+    @Test
+    void findInAsc() {
+        OrderedList<Integer> list = new OrderedList<>(true);
+        list.add(1);
+        list.add(10);
+        list.add(5);
+        list.add(2);
+        list.add(7);
+
+        Node<Integer> integerNode = list.find(5);
+
+        assertEquals(5, integerNode.value);
+    }
+
+    @Test
+    void deleteFromAsc() {
+        OrderedList<Integer> list = new OrderedList<>(true);
+        list.add(1);
+        list.add(10);
+        list.add(5);
+        list.add(5);
+        list.add(2);
+        list.add(7);
+
+        list.delete(5);
+
+        assertEquals(5, list.count());
+        assertNull(list.find(5));
+    }
+
+    @Test
+    void getAll() {
+        OrderedList<Integer> list = new OrderedList<>(true);
+        list.add(1);
+        list.add(10);
+        list.add(5);
+        list.add(2);
+        list.add(7);
+
+        ArrayList<Node<Integer>> all = list.getAll();
+    }
+
+
+    @Test
+    void clear() {
+        OrderedList<Integer> list = new OrderedList<>(true);
+        list.add(1);
+        list.add(10);
+        list.add(5);
+        list.add(2);
+        list.add(7);
+
+        list.clear(true);
+
+        assertNull(list.head);
+        assertNull(list.tail);
+        assertEquals(0, list.count());
+    }
+}
