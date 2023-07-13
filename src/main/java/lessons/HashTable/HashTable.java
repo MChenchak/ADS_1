7109ac2a893c73e1c99b2c8b9c4cc26486296bd1
vtb4055920 <vtb4055920@region.vtb.ru>
2023-07-13 12:04:37 +0300

    add HashTable

---
+++ src/main/java/lessons/HashTable/HashTable.java
@@ -0,0 +1,66 @@
+package lessons.HashTable;
+
+import java.util.Objects;
+
+public class HashTable {
+    public int size;
+    public int step;
+    public String[] slots;
+
+    public HashTable(int sz, int stp) {
+        size = sz;
+        step = stp;
+        slots = new String[size];
+        for (int i = 0; i < size; i++) slots[i] = null;
+    }
+
+    public int hashFun(String value) {
+        if (value == null) {
+            return 0;
+        }
+        int hash = 0;
+        byte[] bytes = value.getBytes();
+        for (byte b : bytes) {
+            hash = hash + b;
+        }
+
+        return hash % size;
+    }
+
+    public int seekSlot(String value) {
+        int hash = hashFun(value);
+
+        while (slots[hash] != null && (hash + step) < size) {
+            hash = hash + step;
+        }
+
+        if (slots[hash] == null) {
+            return hash;
+        }
+
+        return -1;
+    }
+
+    public int put(String value) {
+        int slot = seekSlot(value);
+        if (slot > -1) {
+            slots[slot] = value;
+            return slot;
+        }
+
+        return -1;
+    }
+
+    public int find(String value) {
+        int hash = hashFun(value);
+        while (!Objects.equals(value, slots[hash]) && (hash + step) < size) {
+            hash = hash + step;
+        }
+
+        if (!Objects.equals(value, slots[hash])) {
+            return hash;
+        }
+
+        return -1;
+    }
+}
---
+++ src/test/java/lessons/HashTable/HashTableTest.java
@@ -0,0 +1,55 @@
+package lessons.HashTable;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class HashTableTest {
+
+    @Test
+    void hash() {
+        HashTable hashTable = new HashTable(17, 3);
+        String s = "str";
+
+        int i = hashTable.hashFun(s);
+
+        assertNotEquals(0, i);
+    }
+
+    @Test
+    void put() {
+        HashTable hashTable = new HashTable(5, 3);
+        String s = "str";
+
+        int i = hashTable.put(s);
+
+        assertNotEquals(1, hashTable.size);
+        assertTrue(i >= 0);
+    }
+
+    @Test
+    void seek() {
+        HashTable hashTable = new HashTable(5, 3);
+        String s = "str";
+
+        int i = hashTable.put(s);
+        int i2 = hashTable.put(s);
+
+        assertNotEquals(2, hashTable.size);
+    }
+
+    @Test
+    void find() {
+        HashTable hashTable = new HashTable(5, 3);
+        String s = "str";
+
+        hashTable.put(s);
+        hashTable.put(s);
+
+        int i = hashTable.find(s);
+
+        assertNotEquals(0, i);
+    }
+
+}
